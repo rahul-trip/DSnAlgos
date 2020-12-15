2409e42010c1f0f9d62a15df4ebfe7960618df5d
Thisisretarded <rahul.psit.ec@gmail.com>
2020-12-15 19:55:58 +0530

    Update searchalgos.py
    updated for jump search agorithmn

--- Algorithms/searchalgos.py
+++ Algorithms/searchalgos.py
@@ -32,11 +32,35 @@ def binary_search(x,seq):
 			l = mid+1
 	return False
 
+def jump_search(x,arr):
+	"""
+	Jump search algo implemenation
+	"""
+	print("Jump Search started!")
+	n = len(arr)
+	step = int(n**0.5)
+	arr=sorted(arr)
+	for i in range(0,n,step):
+		
+		if arr[i] < x:
+			continue
+		elif arr[i] == x:
+			return i
+		else:
+			break
+	if i<step:
+		return -1
+	for j in range(i-step, i):
+		if arr[j] == x:
+			return i
+			break
+	else:
+		return -1
 
 if __name__ == "__main__":
 	# Driver code
 	# Linear search
-	l = [1,2,3,4,5,6,7,8,9,0,11,12,13,114,33,44,55,66,77,88,99]
+	l = [0,1,2,3,4,5,6,7,8,9,11,12,13,33,44,55,66,77,88,99,114]
 	x = 1000
 	if linear_search(x,l):
 		print(x, "Found!")
@@ -46,13 +70,21 @@ if __name__ == "__main__":
 	print("-------------------------------------------------")
 	
 	# Binary Search	
-	l = [1,2,3,4,5,6,7,8,9,0,11,12,13,114,33,44,55,66,77,88,99]
+	l = [0,1,2,3,4,5,6,7,8,9,11,12,13,33,44,55,66,77,88,99,114]
 	x = 13
 	if binary_search(x,l):
 		print(x, "Found!")
 	else:
 		print(x, "Not Found!!")
 		
+	print("-------------------------------------------------")
+	l = [0,1,2,3,4,5,6,7,8,9,11,12,13,33,44,55,66,77,88,99,114]
+	x = 13
+	i=jump_search(x,arr)
+	if i == -1:
+		print(x, "Not found")
+	else:
+		print(x, "found at", i, "position")
 	print("-------------------------------------------------")
 	
 	
